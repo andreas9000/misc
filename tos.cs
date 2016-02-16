e41a5083fa095e735c9e5a460b20b3e4325a73e6
andreas9000 <andreas.hansson365@gmail.com>
2016-02-16 19:49:22 +0100

    Create tos.cs

---
+++ tos.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace locurl
+{
+    class tor
+    {
+        public static NetworkStream Stream(string ip, int port, TcpClient t)
+        {
+            string[] split = ip.Split('.');
+            byte[] ipb = new byte[4];
+
+            for (int i = 0; i < 4; i++)
+                ipb[i] = byte.Parse(split[i]);
+
+            t.Connect("localhost", 9050);
+
+            NetworkStream n = t.GetStream();
+
+            byte[] bout = new byte[128];
+            n.Write(new byte[] { 5, 1, 0 }, 0, 3);
+            n.Read(bout, 0, bout.Length);
+
+            if (bout[0] != 5 || bout[1] != 0)
+                throw new Exception();
+
+            n.Write(new byte[] { 5, 1, 0, 1, ipb[0], ipb[1], ipb[2], ipb[3], (byte)(port / 256), (byte)(port % 256) }, 0, 10);
+            n.Read(bout, 0, bout.Length);
+
+            if (bout[0] != 5 || bout[1] != 0)
+                throw new Exception();
+
+            return n;
+        }
+    }
+}
